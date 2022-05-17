fa4eb9f44166f9eddf012a7bc5e5d4a539bcaced
ben <bcaure@gmail.com>
2022-05-17 22:19:09 +0200

    :sparkles: Deploy to Rinkeby test network

--- scripts/run.js
+++ scripts/run.js
@@ -1,24 +1,24 @@
 const main = async () => {
   const [owner, randomPerson] = await hre.ethers.getSigners();
-  const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
+  const waveContractFactory = await hre.ethers.getContractFactory("BenjSmarContract");
   const waveContract = await waveContractFactory.deploy();
   await waveContract.deployed();
 
   console.log("Contract deployed to:", waveContract.address);
   console.log("Contract deployed by:", owner.address);
 
-  let waveCount;
-  waveCount = await waveContract.getTotalWaves();
+  // let waveCount;
+  // waveCount = await waveContract.getTotalWaves({ gasLimit:300000 });
 
-  let waveTxn = await waveContract.wave();
-  await waveTxn.wait();
+  // let waveTxn = await waveContract.wave("hello", { gasLimit:300000 });
+  // await waveTxn.wait();
 
-  waveCount = await waveContract.getTotalWaves();
+  // waveCount = await waveContract.getTotalWaves({ gasLimit:300000 });
 
-  waveTxn = await waveContract.connect(randomPerson).wave();
-  await waveTxn.wait();
+  // waveTxn = await waveContract.connect(randomPerson).wave("hello", { gasLimit:300000 });
+  // await waveTxn.wait();
 
-  waveCount = await waveContract.getTotalWaves();
+  // waveCount = await waveContract.getTotalWaves({ gasLimit:300000 });
 };
 
 const runMain = async () => {
